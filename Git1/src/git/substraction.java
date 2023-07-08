f74ca27bf72513620a7d9ec5baf0e95c74cd5b52
malothuswathi <swathimaloth404@gmail.com>
2023-07-08 18:23:56 +0530

    commit

--- Git1/src/git/substraction.java
+++ Git1/src/git/substraction.java
@@ -3,10 +3,8 @@ package git;
 public class substraction {
 
 	public static void main(String[] args) {
-		int a=2;
-		int b=2;
-		int c=a-b;
-		System.out.println("the sub is: "+c);
+		
+		System.out.println("hi swathi");
 
 	}
 
