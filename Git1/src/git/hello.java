e6c10cbf584b63d5dfdba1af234be0bf4def6a56
malothuswathi <swathimaloth404@gmail.com>
2023-07-08 17:05:37 +0530

    commit4

--- Git1/src/git/hello.java
+++ Git1/src/git/hello.java
@@ -4,7 +4,8 @@ public class hello {
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
-		
+		System.out.println("hi how are you");
+		System.out.println("good eveng");
 	}
 
 
