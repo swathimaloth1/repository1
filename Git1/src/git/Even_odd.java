ff1ef52b6081b6dc5cec9d90bcaa4676af88c579
malothuswathi <swathimaloth404@gmail.com>
2023-07-10 12:11:01 +0530

    commit
    This is even odd class

---
+++ Git1/src/git/Even_odd.java
@@ -0,0 +1,21 @@
+package git;
+import java.util.Scanner;
+
+public class Even_odd {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number");
+	int n=sc.nextInt();
+	if(n%2==0) 
+	{
+		System.out.println("This is the even number");
+	}
+	else
+	{
+		System.out.println("this is the odd number");
+	}
+
+	}
+
+}
