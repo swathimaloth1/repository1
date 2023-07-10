fd52e5a9c698ca32ee1248ee7407cd796f095250
malothuswathi <swathimaloth404@gmail.com>
2023-07-10 14:41:53 +0530

    commit
    commit

--- Git1/src/git1/Example.java
+++ Git1/src/git1/Example.java
@@ -4,7 +4,11 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println("example class");
+<<<<<<< Updated upstream
 		System.out.println();
+=======
+		System.out.println("example ");
+>>>>>>> Stashed changes
 		
 	}
 
